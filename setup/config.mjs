3ff20e8d095027a7d1aa1b9989b41cbfc0a46811
user126179663 <115039826+user126179663@users.noreply.github.com>
2023-11-02 10:29:41 +0900

    Add files via upload

--- setup/config.mjs
+++ setup/config.mjs
@@ -1,4 +1,5 @@
 import { mkdirSync, readdir, readFile, readFileSync, statSync, writeFile } from 'fs';
+import * as readline from 'readline';
 import { basename, dirname, join } from 'path';
 
 class Cascade extends Array {
@@ -164,48 +165,198 @@ Cascade.prototype[Cascade.$merge] = function (method, keys) {
 	
 };
 
-const	path = new Cascade(JSON.parse(process.argv[3])),
-		replace = (target, cascade, outerRx, innerRx) => {
+class Placeholder extends Cascade {
+	
+	static innerRx = /\s*?\[\s*?([^\s\[\]]+?)\s*?\]\s*?/gm;
+	static outerRx = /<\s*?(\[.*?\])\s*?>/gm;
+	
+	constructor(placeholders, outerRx, innerRx) {
+		
+		Array.isArray(placeholders) || (placeholders = [ placeholders ]),
+		
+		super(...placeholders);
+		
+		const { constructor: { innerRx: irx, outerRx: orx } } = this;
+		
+		this.outerRx = outerRx instanceof RegExp ? outerRx : orx,
+		this.innerRx = innerRx instanceof RegExp ? innerRx : irx;
+		
+	}
+	
+	replace(str) {
+		
+		const { innerRx, outerRx } = this, keys = [];
+		let i, v, replaced, lastIndex;
+		
+		i = -1, replaced = '', lastIndex = 0;
+		for (const { 0: outer, 1: outerContent, index: outerIndex } of str.matchAll(outerRx)) {
 			
-			const keys = [];
-			let i, replaced, lastIndex;
+			i = -1, keys.length = 0;
+			for (const { 1: innerContent } of outerContent.matchAll(innerRx)) keys[++i] = innerContent?.trim?.();
 			
-			i = -1, replaced = '', lastIndex = 0;
-			for (const matched of target.matchAll(outerRx)) {
-				
-				i = -1, keys.length = 0;
-				for (const matched0 of matched[1].matchAll(innerRx)) keys[++i] = matched0[1].trim();
-				
-				i === -1 ||	(v = cascade.geti(...keys)) === undefined ||
-					(
-						replaced += target.substring(lastIndex, matched.index) + v,
-						lastIndex = matched.index + matched[0].length
-					);
-				
+			i === -1 ||	(v = this.geti(...keys)) === undefined ||
+				(replaced += str.substring(lastIndex, outerIndex) + v, lastIndex = outerIndex + outer.length);
+			
+		}
+		
+		return replaced + str.substring(lastIndex);
+		
+	}
+	
+}
+
+class Msg extends Cascade {
+	
+	constructor(msgs, placeholders, outerRx, innerRx) {
+		
+		Array.isArray(msgs) || (msgs = [ msgs ]),
+		
+		super(...msgs);
+		
+		this.placeholder =
+			placeholders instanceof Placeholder ? placeholders : new Placeholder(placeholders, outerRx, innerRx);
+		
+	}
+	
+	get() {
+		
+		return this.placeholder.replace(this[Cascade.$get](Cascade.get, false, arguments));
+		
+	}
+	geti() {
+		
+		return this.placeholder.replace(this[Cascade.$get](Cascade.geti, false, arguments));
+		
+	}
+	
+}
+
+class RL extends Msg {
+	
+	constructor(rl, ...args) {
+		
+		super(...args);
+		
+		this.rl = rl || readline.createInterface({ input: process.stdin, output: process.stdout });
+		
+	}
+	
+	close() {
+		
+		this.rl.close();
+		
+	}
+	q(callback) {
+		
+		this.rl.question(this.geti('q'), callback);
+		
+	}
+	w() {
+		
+		this.write(false, arguments);
+		
+	}
+	wi() {
+		
+		this.write(true, arguments);
+		
+	}
+	write(ignoresCase, keys) {
+		
+		this.rl.write(this['get' + (ignoresCase ? 'i' : '')](...keys));
+		
+	}
+	
+}
+
+const	hi = console.log.bind(console, 'hi'),
+		msg = {
+			'ja-JP': {
+				createdFile: 'ファイルを作成しました: "<[path]>"\n',
+				fileExistedPrompt: '\n  "<[path]>"\n\n上記ファイルは既に存在しています。\n\n[別名で新規作成=任意の名前, 作成しない=未入力, 上書き=!]\n',
+				fileExistedPromptDefault: '\n "<[path]>"\n\n上記と同名のファイルが既に存在します。\n\n[ファイル名を変更する=任意の名前, 何もしない=未入力, ファイル名を変更しない=!]\n',
+				noPresetFile: '\nファイル "<[filePath]>" が存在しません。\n同ファイルを使った新規ファイルの作成は中止されます。',
+				q: '>'
 			}
+		},
+		msgData = {},
+		rl = new RL(undefined, [ msg['ja-JP'] ], new Placeholder(msgData)),
+		path = new Placeholder(JSON.parse(process.argv[3])),
+		//place = (target, placeholder, outerRx, innerRx) => {
+		//	
+		//	placeholder instanceof Cascade || (placeholder = new Cascade(placeholder));
+		//	
+		//	const keys = [];
+		//	let i, replaced, lastIndex;
+		//	
+		//	i = -1, replaced = '', lastIndex = 0;
+		//	for (const matched of target.matchAll(outerRx)) {
+		//		
+		//		i = -1, keys.length = 0;
+		//		for (const matched0 of matched[1].matchAll(innerRx)) keys[++i] = matched0[1].trim();
+		//		
+		//		i === -1 ||	(v = placeholder.geti(...keys)) === undefined ||
+		//			(
+		//				replaced += target.substring(lastIndex, matched.index) + v,
+		//				lastIndex = matched.index + matched[0].length
+		//			);
+		//		
+		//	}
+		//	
+		//	return replaced + target.substring(lastIndex);
+		//	
+		//},
+		confirmFile = async (path, option, ...args) => {
 			
-			return replaced + target.substring(lastIndex);
+			typeof option === 'function' && (option = { confirmed: option }),
+			(option && typeof option === 'object') || (option = {}),
+			option.rl || (option.rl = readline.createInterface({ input: process.stdin, output: process.stdout }));
+			
+			const	{ confirmed, existed, rj, rl, rs } = option,
+					confirming =
+						(rs, rj) => {
+							
+							let stat;
+							
+							option.rs = rs, option.rj = rj;
+							
+							try { stat = statSync(path); } catch (error) {}
+							
+							if (stat?.isFile?.()) {
+								
+								typeof existed === 'function' ?
+									existed(path, option, ...args) :
+									(
+										rl.wi('fileExistedPromptDefault'),
+										rl.q	(
+													input =>	input ?
+																	input === '!' ?
+																		confirmed?.(rl, path, option, ...args) :
+																		(
+																			rl.close(),
+																			confirmFile(join(dirname(path), input), option, ...args)
+																		) :
+																	rl.close()
+												)
+									);
+								
+							} else confirmed?.(path, option, ...args);
+							
+						};
+			
+			return rs && rj ? confirming(rs, rj) : new Promise(confirming);
 			
 		},
-		confirm = (path, callback, asFile, ...args) => {
-			
-			let stat;
-			
-			try {
-				
-				stat = statSync(path);
-				
-			} catch (error) {}
-			
-			stat?.['is' + (asFile ? 'File' : 'Directory')]() ?
-				callback(path, ...args) : asFile || callback(mkdirSync(path, { recursive: true }), ...args);
-			
-		},
+		confirmDir =	async (path, callback, ...args) => {
+								
+								try { mkdirSync(path, { recursive: true }); } catch (error) {}
+								
+								await callback(path, ...args);
+								
+							},
 		paths = [ process.cwd() ],
-		settings = new Cascade(),
-		placeholderRx = /<\s*?(\[.*?\])\s*?>/gm,
-		bracketRx = /\s*?\[\s*?([^\s\[\]]+?)\s*?\]\s*?/gm;
-let i,l,i0, k,v, filePath;
+		gwPlaceholder = new Placeholder();
+let i,l,i0, k,v, filePath, file, preset;
 
 v = paths[i = 0];
 while (v !== (v = dirname(v))) paths[++i] = v;
@@ -216,39 +367,69 @@ while (++i < l) {
 	
 	try {
 		
-		v = JSON.parse(replace(readFileSync(join(paths[i], 'gw.placeholder.json'), 'utf8'), path, placeholderRx, bracketRx)),
-		(settings[++i0] = v).path = path[0];
+		v = JSON.parse(path.replace(readFileSync(join(paths[i], 'gw.placeholder.json'), 'utf8'))),
+		(gwPlaceholder[++i0] = v).path = path[0];
 		
 	} catch (error) {}
 	
 }
 
-const preset = settings.mergei('preset');
-console.log(settings);
-for (k in preset) {
+const confirmFileOption = 
+			{
+				confirmed(path, option, ...args) {
+					
+					const { rj, rl, rs } = option;
+					
+					writeFile	(
+										msgData.path = path,
+										gwPlaceholder.replace(file),
+										error => error ?	(console.error(error), rj?.()) :
+																(rl.wi('createdFile'), rs?.())
+									);
+					
+				},
+				existed(path, option, ...args) {
+					
+					const { confirmed, rl, rs } = option;
+					
+					msgData.path = path,
+					
+					rl.wi('fileExistedPromptDefault'),
+					rl.q	(
+								input =>	input ?
+												input === '!' ?
+													confirmed?.(path, option, ...args) :
+													confirmFile(join(dirname(path), input), option, ...args) :
+												rs?.()
+							);
+					
+				},
+				rl
+			},
+		confirmDirCallback =
+			async (path, preset, file) =>
+				await confirmFile	(
+					join(path, basename(preset.path).replace(new RegExp(...preset.renameRx[0]), preset.renameRx[1])),
+					{ ...confirmFileOption }
+				),
+		gwPreset = gwPlaceholder.merge('preset');
+
+for (k in gwPreset) {
 	
-	readFile(
-		filePath = (v = preset[k]).path,
-		'utf8',
-		((preset, filePath) => (error, file) => {
-			
-			if (!error) {
-				
-				confirm(
-					preset.dst,
-					(path, preset) =>
-						writeFile(
-								join(path, basename(filePath).replace(new RegExp(...preset.renameRx[0]), preset.renameRx[1])),
-								replace(file, settings, placeholderRx, bracketRx),
-								error => error && console.error(error)
-							),
-					false,
-					preset
-				);
-				
-			}
-			
-		})(v, filePath)
-	);
+	try {
+		
+		file = readFileSync(msgData.path = (preset = gwPreset[k]).path, 'utf8');
+		
+	} catch (error) {
+		
+		rl.wi('noPresetFile');
+		
+		continue;
+		
+	}
+	
+	await confirmDir(preset.dst, confirmDirCallback, preset, file);
 	
 }
+
+rl.close();
